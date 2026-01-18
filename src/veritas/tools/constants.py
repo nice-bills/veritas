00aad06d5412459aa4d9c32d86a17fa3b7223d9a
Williams <alnicewilliams2@gmail.com>
2026-01-18 23:37:06 +0000

    Centralize contract ABIs and addresses in constants.py

---
+++ src/veritas/tools/constants.py
@@ -0,0 +1,323 @@
+# Constants and ABIs for Veritas Tools
+
+# --- Token Mappings ---
+TOKEN_ADDRESSES_BY_SYMBOLS = {
+    "base-mainnet": {
+        "USDC": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+        "EURC": "0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42",
+        "CBBTC": "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+        "CBETH": "0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22",
+        "WETH": "0x4200000000000000000000000000000000000006",
+        "ZORA": "0x1111111111166b7FE7bd91427724B487980aFc69",
+        "AERO": "0x940181a94a35a4569e4529a3cdfb74e38fd98631",
+        "BNKR": "0x22af33fe49fd1fa80c7149773dde5890d3c76f3b",
+        "CLANKER": "0x1bc0c42215582d5a085795f4badbac3ff36d1bcb",
+    },
+    "base-sepolia": {
+        "USDC": "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+        "EURC": "0x808456652fdb597867f38412077A9182bf77359F",
+        "CBBTC": "0xcbB7C0006F23900c38EB856149F799620fcb8A4a",
+        "WETH": "0x4200000000000000000000000000000000000006",
+    },
+}
+
+# --- Basename Constants ---
+BASENAMES_REGISTRAR_CONTROLLER_ADDRESS_MAINNET = "0x4cCb0BB02FCABA27e82a56646E81d8c5bC4119a5"
+BASENAMES_REGISTRAR_CONTROLLER_ADDRESS_TESTNET = "0x49aE3cC2e3AA768B1e5654f5D3C6002144A59581"
+L2_RESOLVER_ADDRESS_MAINNET = "0xC6d566A56A1aFf6508b41f6c90ff131615583BCD"
+L2_RESOLVER_ADDRESS_TESTNET = "0x6533C94869D28fAA8dF77cc63f9e2b2D6Cf77eBA"
+REGISTRATION_DURATION = 31557600
+
+# --- ABIs ---
+
+ERC20_ABI = [
+    {
+        "type": "function",
+        "name": "balanceOf",
+        "stateMutability": "view",
+        "inputs": [{"name": "account", "type": "address"}],
+        "outputs": [{"type": "uint256"}],
+    },
+    {
+        "type": "function",
+        "name": "transfer",
+        "stateMutability": "nonpayable",
+        "inputs": [{"name": "recipient", "type": "address"}, {"name": "amount", "type": "uint256"}],
+        "outputs": [{"type": "bool"}],
+    },
+    {
+        "type": "function",
+        "name": "approve",
+        "inputs": [{"name": "spender", "type": "address"}, {"name": "amount", "type": "uint256"}],
+        "outputs": [{"type": "bool"}],
+    },
+    {
+        "type": "function",
+        "name": "decimals",
+        "stateMutability": "view",
+        "inputs": [],
+        "outputs": [{"type": "uint8"}],
+    },
+    {
+        "type": "function",
+        "name": "symbol",
+        "stateMutability": "view",
+        "inputs": [],
+        "outputs": [{"type": "string"}],
+    },
+    {
+        "type": "function",
+        "name": "name",
+        "stateMutability": "view",
+        "inputs": [],
+        "outputs": [{"type": "string"}],
+    },
+    {
+        "type": "function",
+        "name": "allowance",
+        "stateMutability": "view",
+        "inputs": [{"name": "owner", "type": "address"}, {"name": "spender", "type": "address"}],
+        "outputs": [{"type": "uint256"}],
+    },
+]
+
+WETH_ABI = [
+    {
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function",
+    },
+    {
+        "inputs": [{"name": "wad", "type": "uint256"}],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [{"name": "account", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
+
+ERC721_ABI = [
+    {
+        "inputs": [{"internalType": "address", "name": "to", "type": "address"}, {"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
+        "name": "mint",
+        "outputs": [],
+        "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "address", "name": "owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"internalType": "uint256", "name": "balance", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
+        "name": "ownerOf",
+        "outputs": [{"internalType": "address", "name": "owner", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "address", "name": "from", "type": "address"},
+            {"internalType": "address", "name": "to", "type": "address"},
+            {"internalType": "uint256", "name": "tokenId", "type": "uint256"},
+        ],
+        "name": "safeTransferFrom",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+]
+
+L2_RESOLVER_ABI = [
+    {
+        "inputs": [
+            {"internalType": "bytes32", "name": "node", "type": "bytes32"},
+            {"internalType": "address", "name": "a", "type": "address"},
+        ],
+        "name": "setAddr",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "bytes32", "name": "node", "type": "bytes32"},
+            {"internalType": "string", "name": "newName", "type": "string"},
+        ],
+        "name": "setName",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+]
+
+REGISTRAR_ABI = [
+    {
+        "inputs": [
+            {
+                "components": [
+                    {"internalType": "string", "name": "name", "type": "string"},
+                    {"internalType": "address", "name": "owner", "type": "address"},
+                    {"internalType": "uint256", "name": "duration", "type": "uint256"},
+                    {"internalType": "address", "name": "resolver", "type": "address"},
+                    {"internalType": "bytes[]", "name": "data", "type": "bytes[]"},
+                    {"internalType": "bool", "name": "reverseRecord", "type": "bool"},
+                ],
+                "internalType": "struct RegistrarController.RegisterRequest",
+                "name": "request",
+                "type": "tuple",
+            }
+        ],
+        "name": "register",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function",
+    }
+]
+
+PYTH_ABI = [
+    {
+        "inputs": [{"internalType": "bytes32", "name": "id", "type": "bytes32"}],
+        "name": "getPrice",
+        "outputs": [
+            {
+                "components": [
+                    {"internalType": "int64", "name": "price", "type": "int64"},
+                    {"internalType": "uint64", "name": "conf", "type": "uint64"},
+                    {"internalType": "int32", "name": "expo", "type": "int32"},
+                    {"internalType": "uint256", "name": "publishTime", "type": "uint256"}
+                ],
+                "internalType": "struct PythStructs.Price",
+                "name": "price",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "bytes32", "name": "id", "type": "bytes32"}],
+        "name": "getPriceUnsafe",
+        "outputs": [
+            {
+                "components": [
+                    {"internalType": "int64", "name": "price", "type": "int64"},
+                    {"internalType": "uint64", "name": "conf", "type": "uint64"},
+                    {"internalType": "int32", "name": "expo", "type": "int32"},
+                    {"internalType": "uint256", "name": "publishTime", "type": "uint256"}
+                ],
+                "internalType": "struct PythStructs.Price",
+                "name": "price",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+# --- Aave Constants ---
+AAVE_POOL_ADDRESSES = {
+    "base-mainnet": "0xa238dd80c259a72e81d7e4664a9801593f98d1c5",
+    "base-sepolia": "0x0000000000000000000000000000000000000000" # TODO: Verify Sepolia
+}
+
+AAVE_POOL_ABI = [
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+            {"internalType": "address", "name": "onBehalfOf", "type": "address"},
+            {"internalType": "uint16", "name": "referralCode", "type": "uint16"},
+        ],
+        "name": "supply",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+            {"internalType": "address", "name": "to", "type": "address"},
+        ],
+        "name": "withdraw",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+            {"internalType": "uint256", "name": "interestRateMode", "type": "uint256"},
+            {"internalType": "uint16", "name": "referralCode", "type": "uint16"},
+            {"internalType": "address", "name": "onBehalfOf", "type": "address"},
+        ],
+        "name": "borrow",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+            {"internalType": "uint256", "name": "interestRateMode", "type": "uint256"},
+            {"internalType": "address", "name": "onBehalfOf", "type": "address"},
+        ],
+        "name": "repay",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+]
+
+# --- Compound Constants ---
+COMET_ADDRESSES = {
+    "base-mainnet": "0xb125E6687d4313864e53df431d5425969c15Eb2F",
+    "base-sepolia": "0x571621Ce60Cebb0c1D442B5afb38B1663C6Bf017",
+}
+
+COMET_ABI = [
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+        ],
+        "name": "supply",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [
+            {"internalType": "address", "name": "asset", "type": "address"},
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "address", "name": "account", "type": "address"}],
+        "name": "borrowBalanceOf",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
