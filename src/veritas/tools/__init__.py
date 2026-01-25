d584c6b06d136bab447c3aa9d1b3566a23b209bd
Williams <alnicewilliams2@gmail.com>
2026-01-25 17:50:44 +0000

    Export ChainlinkCapability in tools module
    - Added ChainlinkCapability to module exports
    - Makes new price feed capability available to agents

--- src/veritas/tools/__init__.py
+++ src/veritas/tools/__init__.py
@@ -2,16 +2,16 @@ from .base import VeritasTool, VeritasCapability, WalletCapability, TradeCapabil
 from .token import TokenCapability
 from .nft import ERC721Capability, BasenameCapability
 from .defi import AaveCapability, CompoundCapability
-from .infra import PythCapability, OnrampCapability
+from .infra import PythCapability, OnrampCapability, ChainlinkCapability
 from .social import SocialCapability
 from .payments import PaymentCapability
 from .wow import CreatorCapability
 from .nillion import PrivacyCapability
 
 __all__ = [
-    "VeritasTool", 
-    "VeritasCapability", 
-    "WalletCapability", 
+    "VeritasTool",
+    "VeritasCapability",
+    "WalletCapability",
     "TradeCapability",
     "TokenCapability",
     "ERC721Capability",
@@ -20,8 +20,9 @@ __all__ = [
     "CompoundCapability",
     "PythCapability",
     "OnrampCapability",
-    "SocialCapability", 
+    "ChainlinkCapability",
+    "SocialCapability",
     "PaymentCapability",
     "CreatorCapability",
-    "PrivacyCapability"
+    "PrivacyCapability",
 ]
